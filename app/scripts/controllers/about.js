d2e626349efb0c9ee73fe5850bea699421233f51
Joel Rahman <joelrahman@Joels-Laptop.local>
2016-01-22 09:40:57 +1100

    Scaffolded

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spatialAngularSampleAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the spatialAngularSampleAppApp
+ */
+angular.module('spatialAngularSampleAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spatialAngularSampleAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the spatialAngularSampleAppApp
+ */
+angular.module('spatialAngularSampleAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
