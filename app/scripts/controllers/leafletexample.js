29fe0e259962239f96a534630028d58d7a7ef6e9
Joel Rahman <joel@flowmatters.com.au>
2016-01-22 10:36:11 +1100

    Added WALD WMS layer example to Leaflet

--- app/scripts/controllers/leafletexample.js
+++ app/scripts/controllers/leafletexample.js
@@ -23,12 +23,36 @@ angular.module('spatialAngularSampleAppApp')
         baselayers:{
           osm: {
             name: 'OpenStreetMap',
-            url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            type: 'xyz'
+            url: 'http://129.206.228.72/cached/osm?',
+            type: 'wms',
+            layerParams:{
+              version: '1.1.1',
+              format: 'image/png',
+              layers:'osm_auto:all'
+            }
           },
         },
         overlays:{
+          aWMS:{
+            // http://dapds00.nci.org.au/thredds/wms/ub8/global/nc/1d/actual/Stot/20150920_Stot_daily.nc?service=WMS&version=1.3.0&request=GetCapabilities
+            name: 'Some Model Results',
+            type: 'wms',
+            visible: true,
+            url: 'http://dapds00.nci.org.au/thredds/wms/ub8/global/nc/1d/actual/Stot/20150920_Stot_daily.nc?',
+            doRefresh: true,
+            layerParams: {
+              layers: 'Stot_daily',
+              time:'2015-09-20',
+              format: 'image/png',
+              transparent: true,
+              logscale:false,
+              styles:'boxfill/rainbow',
+              numcolorbands:50,
+              colorscalerange:'0,1000',
+              belowmincolor:'extend',abovemaxcolor:'extend'
+            }
 
+          }
         }
       }
     });
